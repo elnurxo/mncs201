d9b13e7b165e9273cab900e5ba954b3610a022f9
elnurxo <elnurykh@code.edu.az>
2024-10-30 14:09:49 +0400

    js: arrays

---
+++ 03. lesson/script.js
@@ -0,0 +1,85 @@
+//let var const
+//global scope
+
+//Pascal, camelCase, snake_case, kebab-case (html)
+
+// let temp = 17;
+
+// > 20 < 30 mid
+// < 20 cold
+// > 30 hot
+
+console.log(true || (5 > 4 && !false && true) || ((!true || 6 != 6) && false));
+
+//condition statement
+// if, else, else if, switch - case
+
+// let fullName = "Eli Agayev";
+// let age = 18;
+// let balance = 7;
+// let ticketPrice = 8;
+
+// if (age > 18) {
+//   console.log("welcome to the cinema!");
+//   //nested conditions
+//   if (balance >= ticketPrice) {
+//     balance -= ticketPrice;
+//     console.log(`current balance is: ${balance}`);
+//   } else {
+//     console.log("not enough balance!");
+//   }
+// } else if (age == 18) {
+//   console.log("welcome to the cinema!");
+//   balance -= ticketPrice / 2;
+//   console.log(`current balance is: ${balance}`);
+// } else {
+//   console.log("cix cole!");
+// }
+
+//scenario 2
+
+// let userName = 'ravafm';
+// let password = 'User123!';
+// if (userName == 'ravanfm' && password == 'User123!'){
+//     console.log(`welcome ${userName}` )
+// }
+// else if (userName != 'ravanfm'){
+//     console.log('invaild userName')
+
+// }
+// else {
+//     console.log('invalid password')
+// }
+
+// let weekDay = "saturday";
+
+// switch (weekDay) {
+//   case "monday":
+//     console.log(1);
+//     break;
+//   case "tuesday":
+//     console.log(2);
+//     break;
+//   case "wednesday":
+//     console.log(3);
+//     break;
+//   case "thursday":
+//     console.log(4);
+//     break;
+//   case "friday":
+//     console.log(5);
+//     break;
+//   case "saturday":
+//   case "sunday":
+//     console.log("weekend");
+//     break;
+//   default:
+//     console.log("invalid input");
+//     break;
+// }
+
+// let numbers = [1, 2, 3, 4];
+
+//push, pop, shift, unshift
+// numbers.splice(2, 1);
+// console.log(numbers);
